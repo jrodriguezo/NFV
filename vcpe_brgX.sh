ec7681a095e3372bb06b4fe8a5fba9683b5ab23d
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2020-12-31 17:31:25 +0100

    Test 2

--- vcpe_brgX.sh
+++ vcpe_brgX.sh
@@ -16,7 +16,16 @@
 #sudo docker exec -it $VNF1 ovs-vsctl add-port br0 eth1
 #sudo docker exec -it $VNF1 ifconfig eth1 up
 
+# antes de hacer el dhclient es necesario que el ryu-manager esté corriendo
 
-sed '/OFPFlowMod(/,/)/s/)/, table_id=1)/' /usr/lib/python3/dist-packages/ryu/app/simple_switch_13.py > qos_simple_switch_13.py
+#sed '/OFPFlowMod(/,/)/s/)/, table_id=1)/' /usr/lib/python3/dist-packages/ryu/app/simple_switch_13.py > qos_simple_switch_13.py
 
-ryu-manager ryu.app.rest_qos ryu.app.rest_conf_switch ./qos_simple_switch_13.py
+#ryu-manager ryu.app.rest_qos ryu.app.rest_conf_switch ./qos_simple_switch_13.py
+
+#hacer los curls en un terminal a parte
+
+# curl -X PUT -d '"tcp:127.0.0.1:6632"' http://10.250.0.22:8080/v1.0/conf/switches/0000000000000001/ovsdb_addr
+# curl -X POST -d '{"port_name": "vxlan1", "type": "linux-htb", "max_rate": "6000000", "queues": [{"max_rate": "2000000"}, {"min_rate": "2000000"}]}' http://10.250.0.22:8080/qos/queue/0000000000000001
+# curl -X POST -d '{"match": {"nw_dst": "192.168.255.1", "nw_proto": "UDP", "tp_dst": "3000"}, "actions":{"queue": "1"}}' http://10.250.0.22:8080/qos/rules/0000000000000001
+### curl -X POST -d '{"match": {"nw_dst": "192.168.255.1", "nw_proto": "UDP", "udp_dst": "3000"}, "actions":{"queue": "1"}}' http://10.250.0.22:8080/qos/rules/0000000000000001
+# curl -X GET http://10.250.0.22:8080/qos/rules/0000000000000001
